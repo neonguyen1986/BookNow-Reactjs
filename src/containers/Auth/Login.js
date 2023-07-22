9d0148b3e4b28a8a0091f161c49c56af1df0bc15
Neo Nguyen <neonguyen.ca@gmail.com>
2023-07-22 15:55:49 -0600

    2. Create Login Responsive Form

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -35,6 +35,7 @@ class Login extends Component {
       isShowPassword: !this.state.isShowPassword
     })
   }
+
   render() {
     let { username, password, isShowPassword } = this.state;
     return (
