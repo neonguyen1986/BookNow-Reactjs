d33d5e43d193e48fb52b21bd721a0be3c4132c8b
Neo Nguyen <neonguyen.ca@gmail.com>
2023-07-31 16:53:13 -0600

    10. Input Avatar Image

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
-import { getAllCodeService } from '../../../services/userService';
 import { LANGUAGE } from '../../../utils'
 import * as actions from "../../../store/actions"
+import './UserRedux.scss'
+
+//Lightbox
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
+
 
 class UserRedux extends Component {
 
@@ -12,7 +17,9 @@ class UserRedux extends Component {
         this.state = {
             genderArr: [],
             positionArr: [],
-            roleArr: []
+            roleArr: [],
+            previewIgmURL: '',
+            isOpen: false,
         }
     }
 
@@ -39,12 +46,31 @@ class UserRedux extends Component {
             })
     }
 
+    handleOnChangeImage = (e) => {
+        let data = e.target.files;
+        let file = data[0];
+        if (file) {
+            //Ta sẽ tạo một API (của HTML) của ảnh này
+            let objectUrl = URL.createObjectURL(file)
+            this.setState({
+                previewIgmURL: objectUrl
+            })
+        }
+        // console.log('>>>check image:', objectUrl)
+    }
+    handleOpenPreviewImage = () => {
+        if (!this.state.previewIgmURL) return;
+        this.setState({
+            isOpen: true
+        })
+    }
+
     render() {
         // let { genderArr } = this.state;
         let language = this.props.language
-        let { genderArr, positionArr, roleArr } = this.state
+        let { genderArr, positionArr, roleArr, previewIgmURL, isOpen } = this.state
         let isLoadingGenderReact = this.props.isLoadingGenderRedux
-        console.log('>>>check position', this.state)
+        console.log('>>>check image:', previewIgmURL)
         return (
             <div className="user-redux-container" >
                 <div className='title'>
@@ -54,78 +80,97 @@ class UserRedux extends Component {
                     ?
                     <div> Loading...</div>
                     :
-                    <div className='user-redux-body'>
-                        <div className='container'>{/* giúp chừa khoảng trống 2 bên content*/}
-                            <div className='col-12 my-3'>
-                                <FormattedMessage id='manage-user.add' />
-                            </div>
-                            <div className='row'>
-                                <div className='col-6'>
-                                    <label><FormattedMessage id='manage-user.email' /></label>
-                                    <input className='form-control' type='email' />
+                    <>
+                        <div className='user-redux-body'>
+                            <div className='container'>{/* giúp chừa khoảng trống 2 bên content*/}
+                                <div className='col-12 my-3'>
+                                    <FormattedMessage id='manage-user.add' />
                                 </div>
-                                <div className='col-6'>
-                                    <label><FormattedMessage id='manage-user.password' /></label>
-                                    <input className='form-control' type='password' />
+                                <div className='row'>
+                                    <div className='col-6'>
+                                        <label><FormattedMessage id='manage-user.email' /></label>
+                                        <input className='form-control' type='email' />
+                                    </div>
+                                    <div className='col-6'>
+                                        <label><FormattedMessage id='manage-user.password' /></label>
+                                        <input className='form-control' type='password' />
+                                    </div>
+                                    <div className='col-6'>
+                                        <label><FormattedMessage id='manage-user.first-name' /></label>
+                                        <input className='form-control' type='text' />
+                                    </div>
+                                    <div className='col-6'>
+                                        <label><FormattedMessage id='manage-user.last-name' /></label>
+                                        <input className='form-control' type='text' />
+                                    </div>
+                                    <div className='col-3'>
+                                        <label><FormattedMessage id='manage-user.phone-number' /></label>
+                                        <input className='form-control' type='text' />
+                                    </div>
+                                    <div className='col-9'>
+                                        <label><FormattedMessage id='manage-user.address' /></label>
+                                        <input className='form-control' type='text' />
+                                    </div>
+                                    <div className='col-3'>
+                                        <label><FormattedMessage id='manage-user.gender' /></label>
+                                        <select className="form-control">
+                                            {genderArr && genderArr.length > 0 &&
+                                                genderArr.map(item => {
+                                                    return (
+                                                        <option key={item.id}>{language === LANGUAGE.VI ? item.valueVi : item.valueEn}</option>
+                                                    )
+                                                })}
+                                        </select>
+                                    </div>
+                                    <div className='col-3'>
+                                        <label><FormattedMessage id='manage-user.position' /></label>
+                                        <select className="form-control">
+                                            {positionArr?.length > 0 &&
+                                                positionArr.map(item => {
+                                                    return (
+                                                        <option key={item.id}>{language === LANGUAGE.VI ? item.valueVi : item.valueEn}</option>
+                                                    )
+                                                })}
+                                        </select>
+                                    </div>
+                                    <div className='col-3'>
+                                        <label><FormattedMessage id='manage-user.role-id' /></label>
+                                        <select className="form-control">
+                                            {roleArr?.length > 0 &&
+                                                roleArr.map(item => {
+                                                    return (
+                                                        <option key={item.id}>{language === LANGUAGE.VI ? item.valueVi : item.valueEn}</option>
+                                                    )
+                                                })}
+                                        </select>
+                                    </div>
+                                    <div className='col-3'>
+                                        <label><FormattedMessage id='manage-user.image' /></label>
+                                        <div className='preview-image-container'>
+                                            <input id='previewImg' type='file' hidden
+                                                onChange={(e) => this.handleOnChangeImage(e)}
+                                            />
+                                            <label className='upload-label' htmlFor='previewImg'>Image <i class="fas fa-upload"></i></label>
+                                            {/* htmlFor phải trùng với id của input, khi này 
+                                            label này sẽ đại diện cho thẻ input ở trên */}
+                                            <div className='preview-image'
+                                                style={{ backgroundImage: `url(${previewIgmURL}` }}
+                                                onClick={() => this.handleOpenPreviewImage()}
+                                            ></div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className='col-6'>
-                                    <label><FormattedMessage id='manage-user.first-name' /></label>
-                                    <input className='form-control' type='text' />
-                                </div>
-                                <div className='col-6'>
-                                    <label><FormattedMessage id='manage-user.last-name' /></label>
-                                    <input className='form-control' type='text' />
-                                </div>
-                                <div className='col-3'>
-                                    <label><FormattedMessage id='manage-user.phone-number' /></label>
-                                    <input className='form-control' type='text' />
-                                </div>
-                                <div className='col-9'>
-                                    <label><FormattedMessage id='manage-user.address' /></label>
-                                    <input className='form-control' type='text' />
-                                </div>
-                                <div className='col-3'>
-                                    <label><FormattedMessage id='manage-user.gender' /></label>
-                                    <select className="form-control">
-                                        {genderArr && genderArr.length > 0 &&
-                                            genderArr.map(item => {
-                                                return (
-                                                    <option key={item.id}>{language === LANGUAGE.VI ? item.valueVi : item.valueEn}</option>
-                                                )
-                                            })}
-                                    </select>
-                                </div>
-                                <div className='col-3'>
-                                    <label><FormattedMessage id='manage-user.position' /></label>
-                                    <select className="form-control">
-                                        {positionArr?.length > 0 &&
-                                            positionArr.map(item => {
-                                                return (
-                                                    <option key={item.id}>{language === LANGUAGE.VI ? item.valueVi : item.valueEn}</option>
-                                                )
-                                            })}
-                                    </select>
-                                </div>
-                                <div className='col-3'>
-                                    <label><FormattedMessage id='manage-user.role-id' /></label>
-                                    <select className="form-control">
-                                        {roleArr?.length > 0 &&
-                                            roleArr.map(item => {
-                                                return (
-                                                    <option key={item.id}>{language === LANGUAGE.VI ? item.valueVi : item.valueEn}</option>
-                                                )
-                                            })}
-                                    </select>
-                                </div>
-                                <div className='col-3'>
-                                    <label><FormattedMessage id='manage-user.image' /></label>
-                                    <input className='form-control' type='text' />
-                                </div>
-                            </div>
-                            <button className='btn btn-primary my-3'><FormattedMessage id='manage-user.save' /></button>
+                                <button className='btn btn-primary my-3'><FormattedMessage id='manage-user.save' /></button>
 
+                            </div>
                         </div>
-                    </div>
+                        {isOpen === true &&
+                            <Lightbox
+                                mainSrc={previewIgmURL}
+                                onCloseRequest={() => this.setState({ isOpen: false })}
+                            />
+                        }
+                    </>
                 }
             </div>
         )
